634026a8c874e32cba465b926a43c3ed9238b1b0
VRUSHABH SOMANI <vssomani2002@gmail.com>
2024-08-13 11:09:41 +0530

    Default Configuration

--- stepper/src/App.js
+++ stepper/src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
-import "./style.css";
+import "./styles.css";
+import Stepper from "./components/stepper";
+
+const CHECKOUT_STEPS = [
+  {
+    name: "Customer Info",
+    Component: () => <div>Provide your contact details.</div>,
+  },
+  {
+    name: "Shipping Info",
+    Component: () => <div>Enter your shipping address.</div>,
+  },
+  {
+    name: "Payment",
+    Component: () => <div>Complete payment for your order.</div>,
+  },
+  {
+    name: "Delivered",
+    Component: () => <div> Your order has been delivered.</div>,
+  },
+];
+
+
 function App() {
-  return
+  return(
   <div>
-    
+    <h2>CheckOut VS </h2>
+    <Stepper stepsConfig={CHECKOUT_STEPS} />
   </div>
+  );
 }
 
 export default App;
